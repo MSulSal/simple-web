d688722d0784476a1c3dad8079102cfd19e233f5
Suleman <msulsaleem@gmail.com>
2024-12-03 16:23:11 -0600

    date-time, client port

--- server.py
+++ server.py
@@ -1,24 +1,48 @@
 import http.server
 
 class RequestHandler(http.server.BaseHTTPRequestHandler):
-    '''Handle HTTP requests by returning a fixed 'page'.'''
+    '''Handle HTTP requests by returning a fixed "page"'''
     
     # Page to send back.
     Page = '''\
 <html>
 <body>
-<p>Hello, web!</p>
+<table>
+<tr> <td>Header</td> <td>Value</td> </tr>
+<tr> <td>Date and time</td> <td>{date_time}</td> </tr>
+<tr> <td>Client host</td> <td>{client_host}</td> </tr>
+<tr> <td>Client port</td> <td>{client_port}s</td> </tr>
+<tr> <td>Command</td> <td>{command}</td> </tr>
+<tr> <td>Path</td> <td>{path}</td> </tr>
+</table>
 </body>
 </html>
 '''
 
     # Handle a GET request.
     def do_GET(self):
+        self.send_page(self.create_page())
+
+    def create_page(self):
+        values = {
+            "date_time": self.date_time_string(),
+            "client_host": self.client_address[0],
+            "client_port": self.client_address[1],
+            "command": self.command,
+            "path": self.path
+        }
+
+        page = self.Page.format(**values).encode("utf-8")
+        return page
+
+    def send_page(self, page):
         self.send_response(200)
-        self.send_header("Content-Type", "text/html")
-        self.send_header("Content-Length", str(len(self.Page)))
+        self.send_header("Content-type", "text/html")
+        self.send_header("Content-length", str(len(page)))
         self.end_headers()
-        self.wfile.write(self.Page.encode('utf-8'))  # Ensure encoding to bytes
+        self.wfile.write(page)
+
+
 
 #----------------------------------------------------------------------
 
