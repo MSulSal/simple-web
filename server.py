fbf5007d076e219e5dd9f882ee72c7c3bb510e42
Suleman <msulsaleem@gmail.com>
2024-12-03 20:37:30 -0600

    file handling

--- server.py
+++ server.py
@@ -1,48 +1,102 @@
 import http.server
+import os
+
+class ServerException(Exception):
+    """Custom exception to represent server-related errors."""
+    def __init__(self, message):
+        super().__init__(message)
+
 
 class RequestHandler(http.server.BaseHTTPRequestHandler):
     '''Handle HTTP requests by returning a fixed "page"'''
     
-    # Page to send back.
-    Page = '''\
+    # page to send back.
+#     Page = '''\
+# <html>
+# <body>
+# <table>
+# <tr> <td>Header</td> <td>Value</td> </tr>
+# <tr> <td>Date and time</td> <td>{date_time}</td> </tr>
+# <tr> <td>Client host</td> <td>{client_host}</td> </tr>
+# <tr> <td>Client port</td> <td>{client_port}s</td> </tr>
+# <tr> <td>Command</td> <td>{command}</td> </tr>
+# <tr> <td>Path</td> <td>{path}</td> </tr>
+# </table>
+# </body>
+# </html>
+# '''
+
+    # error page
+    Error_Page = '''\
 <html>
 <body>
-<table>
-<tr> <td>Header</td> <td>Value</td> </tr>
-<tr> <td>Date and time</td> <td>{date_time}</td> </tr>
-<tr> <td>Client host</td> <td>{client_host}</td> </tr>
-<tr> <td>Client port</td> <td>{client_port}s</td> </tr>
-<tr> <td>Command</td> <td>{command}</td> </tr>
-<tr> <td>Path</td> <td>{path}</td> </tr>
-</table>
+<h1>Error accessing {path}</h1>
+<p>{msg}</p>
 </body>
 </html>
 '''
 
-    # Handle a GET request.
+    # handle a GET request.
     def do_GET(self):
-        self.send_page(self.create_page())
+        try:
+            # figure out what exactly is being requested
+            full_path = os.getcwd() + self.path
 
-    def create_page(self):
-        values = {
-            "date_time": self.date_time_string(),
-            "client_host": self.client_address[0],
-            "client_port": self.client_address[1],
-            "command": self.command,
-            "path": self.path
-        }
+            # it doesn't exist...
+            if not os.path.exists(full_path):
+                raise ServerException("'{0}' not found".format(self.path))
+            
+            # ...it's a file...
+            elif os.path.isfile(full_path):
+                self.handle_file(full_path)
 
-        page = self.Page.format(**values).encode("utf-8")
-        return page
+            # ...it's something we don't handle
+            else:
+                raise ServerException("Unkown object '{0}'".format(self.path))
+        
+        # handle errors
+        except Exception as msg:
+            self.handle_error(msg)
 
-    def send_page(self, page):
-        self.send_response(200)
+    def handle_file(self, full_path):
+        try:
+            with open(full_path, "rb") as reader:
+                content = reader.read()
+            self.send_content(content)
+        except IOError as msg:
+            msg = "'{0}' cannot be read: {1}".format(self.path, msg)
+
+    # def create_page(self):
+    #     values = {
+    #         "date_time": self.date_time_string(),
+    #         "client_host": self.client_address[0],
+    #         "client_port": self.client_address[1],
+    #         "command": self.command,
+    #         "path": self.path
+    #     }
+
+    #     page = self.Page.format(**values).encode("utf-8")
+    #     return page
+
+    # def send_page(self, page):
+    #     self.send_response(200)
+    #     self.send_header("Content-type", "text/html")
+    #     self.send_header("Content-length", str(len(page)))
+    #     self.end_headers()
+    #     self.wfile.write(page)
+
+    # send actual content
+    def send_content(self, content, status=200):
+        self.send_response(status)
         self.send_header("Content-type", "text/html")
-        self.send_header("Content-length", str(len(page)))
+        self.send_header("Content-length", str(len(content)))
         self.end_headers()
-        self.wfile.write(page)
+        self.wfile.write(content)
 
-
+    # handle unknown objects
+    def handle_error(self, msg):
+        content = self.Error_Page.format(path=self.path, msg=msg)
+        self.send_content(content, 404)
 
 #----------------------------------------------------------------------
 
